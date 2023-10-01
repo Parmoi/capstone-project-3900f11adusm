de9f7a66eb79cd6ebf876228168f40132d0fe43e
Uguudei <auudguu@gmail.com>
2023-10-01 22:49:03 +1100

    created a basic setup for our project

---
+++ frontend/src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+const App = () => {
+ return <h1>The CollectiblesCorner!</h1>;
+ }
+
+ReactDOM.render(<App />, document.getElementById('app'));
