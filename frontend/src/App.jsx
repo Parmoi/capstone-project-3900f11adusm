688c414ee4a1cb79c316a38a2012a0a06d20d292
Stella Zhang <zstella159@gmail.com>
2023-10-04 01:07:57 +1100

    Added template ui for registering and signing up and nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+// import IconButton from '@mui/material/IconButton';
+
+import SignIn from './components/SignIn';
+import Register from './components/Register';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  // useParams,
+  // useNavigate,
+  // Outlet,
+} from 'react-router-dom';
+
+const SignedOutNav = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
+          <Typography variant="h6" component="div">
+          🧸 CollectiblesCorner
+          </Typography>
+          <Box>
+            <Button color="inherit"><Link to="/login">Login</Link></Button>
+            <Button color="inherit"><Link to="/register">Register</Link></Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reloaddsad.jdsajl;
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+    <BrowserRouter>
+      <SignedOutNav />
+      <Routes>
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/register" element={<Register />}/>
+      </Routes>
+    </BrowserRouter>
+    </Box>
   );
 }
 
---
+++ frontend/src/components/Register.jsx
@@ -0,0 +1,107 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useNavigate } from 'react-router-dom';
+// import validator from 'validator';
+
+const theme = createTheme();
+
+function Register () {
+  const navigate = useNavigate();
+
+//   const validateEmail = (e) => {
+//     const email = e.target.value;
+//     setEmailError(!validator.isEmail(email));
+//   };
+
+//   const validateName = (e) => {
+//     const name = e.target.value;
+//     setNameError(name === '');
+//   };
+
+//   const validatePwd = (e) => {
+//     const pwd = e.target.value;
+//     setPwdError(pwd === '');
+//   };
+
+//   const register = (e) => {
+   
+//   }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            {/* <LockOutlinedIcon /> */}
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="name"
+              label="Name"
+              type="name"
+              id="name"
+              autoComplete="name"
+            //   onBlur={(e) => validateName(e)}
+            //   helperText={nameError ? 'Name must not be empty' : ''}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            //   onChange={(e) => validateEmail(e)}
+            //   helperText={emailError ? 'Email must be valid' : ''}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="text"
+              id="password"
+              autoComplete="current-password"
+            //   onBlur={(e) => validatePwd(e)}
+            //   helperText={pwdError ? 'Password must not be empty' : ''}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Register
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default Register;
