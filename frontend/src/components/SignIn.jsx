d4e254916d331ce78851a8ab20caef937fdc7884
Stella Zhang <zstella159@gmail.com>
2023-10-04 14:21:03 +1100

    Added stub for login submit function

--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -11,64 +11,71 @@ import { useNavigate } from 'react-router-dom';
 
 const theme = createTheme();
 
-function SignIn () { 
-  
-    return (
-      <ThemeProvider theme={theme}>
-        <Container component="main" maxWidth="xs">
-          <CssBaseline />
-          <Box
-            sx={{
-              marginTop: 8,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
-            <Box component="form" noValidate sx={{ mt: 1 }}>
-              {/* <div>
+function SignIn() {
+
+  const login = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.currentTarget);
+
+    console.log(data);
+    // login api call
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={login} noValidate sx={{ mt: 1 }}>
+            {/* <div>
                 {error ? <Alert severity='error'>{errContent}</Alert> : <></> }
               </div> */}
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="text"
-                id="password"
-                autoComplete="current-password"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-                name="sign in"
-              >
-                Sign In
-              </Button>
-            </Box>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="text"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              name="sign in"
+            >
+              Sign In
+            </Button>
           </Box>
-        </Container>
-      </ThemeProvider>
-    );
-  }
-  
-  export default SignIn;
-  
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default SignIn;
