81d57a8779ab8d3d39e2c9843e285a38b081d374
Dyllanson So <dyllanson.so@student.unsw.edu.au>
2023-10-10 18:06:10 +1100

    Changes to db_manager to be used in Docker

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 import os
+import db_manager as dbm
 
 app = Flask(__name__)
 password = os.environ['POSTGRES_PASSWORD']
@@ -37,38 +38,15 @@ def get_widgets():
 
 @app.route('/initdb')
 def db_init():
-    conn = psycopg2.connect(
-        host="db",
-        user="postgres",
-        password=password,
-    )
-    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    dbm.database_setup()
 
-    cursor = conn.cursor()
+    return 'Database has been setup successfully!'
 
-    cursor.execute("DROP DATABASE IF EXISTS example")
-    cursor.execute("CREATE DATABASE example")
+@app.route('/insertcollector')
+def db_collector_insert():
+    dbm.insert_collector("bob@gmail.com", "bob", "jacob", "password")
 
-    cursor.close()
-    conn.commit()
-    conn.close()
-
-    conn = psycopg2.connect(
-    host="db",
-    user="postgres",
-    password=password,
-    database="example"
-    )
-    cursor = conn.cursor()
-
-    cursor.execute("DROP TABLE IF EXISTS widgets")
-    cursor.execute("CREATE TABLE widgets (name VARCHAR(255), description VARCHAR(255))")
-
-    cursor.close()
-    conn.commit()
-    conn.close()
-
-    return 'init database'
+    return 'Insert has been successful!'
 
 if __name__ == "__main__":
     app.run(host ='0.0.0.0')
--- backend/db_manager.py
+++ backend/db_manager.py
@@ -1,40 +1,31 @@
 import sqlalchemy as db
 import psycopg2
+import os
 
-# Notes:
-# - Database is called "collectible_exchange_db"
-# - If table already exists, running the table code won't change it. We have to 
-#   delete it first
-# - Currently we don't have password hashing just yet
-# - Tokens have not been implemented yet either
+db_name = "collectibles_db"
+db_user = "postgres"
+db_password = os.environ['POSTGRES_PASSWORD']
+db_host = "db"
 
-db_name = "collectible_exchange_db"
-
-# Function to setup our db for the first time
+# Function to setup our database
 def database_setup():
 
     # Establish connection
     conn = psycopg2.connect(
-        database="postgres",
-        user="postgres",
-        password="password",
-        host="localhost"
+        user=db_user,
+        password=db_password,
+        host=db_host
     )
-
     conn.autocommit = True
 
     # Use cursor to create our database if it doesn't exist
     cursor = conn.cursor()
-    stmt = f"SELECT * FROM pg_database WHERE datname = '{db_name}'"
-    cursor.execute(stmt)
-    result = cursor.fetchall()
-    if (len(result) == 0):
-        create_stmt = f"CREATE DATABASE {db_name}"
-        cursor.execute(create_stmt)
+    cursor.execute(f"DROP DATABASE IF EXISTS {db_name} WITH (FORCE)")
+    cursor.execute(f"CREATE DATABASE {db_name}")
     conn.close()
 
-    # Connect to newly made database again
-    engine = db.create_engine(f"postgresql://postgres:password@localhost/{db_name}")
+    # Connect to database again
+    engine = db.create_engine(f"postgresql://{db_user}:{db_password}@{db_host}/{db_name}")
     conn = engine.connect()
     metadata = db.MetaData()
 
@@ -51,13 +42,14 @@ def database_setup():
 
     # Creates all tables stored within metadata
     metadata.create_all(engine)
+    conn.close()
 
 # Function to insert collector into our db
 def insert_collector(email, first_name, last_name, password):
 
     # Create an engine and connect to the db
     engine = db.create_engine(
-        f"postgresql://postgres:password@localhost/{db_name}").execution_options(
+        f"postgresql://{db_user}:{db_password}@{db_host}/{db_name}").execution_options(
             isolation_level="AUTOCOMMIT")
     conn = engine.connect()
     metadata = db.MetaData()
@@ -73,7 +65,4 @@ def insert_collector(email, first_name, last_name, password):
          "password": password}
         )
     conn.execute(insert_stmt)
-
-# Code for testing purposes
-database_setup()
-insert_collector("hello22222@gmail.com", "peter", "bobby", "password")
+    conn.close()
