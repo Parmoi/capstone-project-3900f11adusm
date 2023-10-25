dd26020bba4ef9c020e6403f1aaf33d3376e4fce
Stella Zhang <zstella159@gmail.com>
2023-10-25 20:25:51 +1100

    Attempted to connect collection routes to frontend

--- frontend/src/pages/CollectionList.jsx
+++ frontend/src/pages/CollectionList.jsx
@@ -2,11 +2,11 @@ import React, { useMemo, Fragment } from 'react';
 
 import {
 
-    MaterialReactTable,
+  MaterialReactTable,
 
-    MRT_ToggleDensePaddingButton,
+  MRT_ToggleDensePaddingButton,
 
-    MRT_FullScreenToggleButton,
+  MRT_FullScreenToggleButton,
 
 } from 'material-react-table';
 
@@ -17,6 +17,7 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import moment from 'moment';
+import { apiCall } from '../App';
 
 // data stub waiting for backend
 const stubData = [
@@ -63,10 +64,35 @@ const stubData = [
     dateAdded: '3/2/2014',
   },
 ]
-  
+
 // sourced from https://github.com/KevinVandy/material-react-table/blob/v1/apps/material-react-table-docs/examples/custom-top-toolbar/sandbox/src/JS.js
-const CollectionList = () => {
-  const [data, setData] = React.useState(stubData);
+function CollectionList() {
+  const [data, setData] = React.useState([]);
+
+  const fetchData = () => {
+    console.log('fetching data');
+    // call api with data
+    const options = {
+      method: 'GET',
+      route: "/collection/get",
+    };
+
+    apiCall((d) => {
+      console.log(d);
+      setData(d);
+    }, options)
+      .then((res) => {
+        if (res) {
+          // set error msg if api call returns error
+
+        }
+      });
+  }
+
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+
   const columns = useMemo(
     //column definitions...
     () => [
@@ -103,67 +129,67 @@ const CollectionList = () => {
       },
       {
         accessorFn: (row) => moment(row.dateReleased, "DD/MM/YYYY"), //convert to Date for sorting and filtering
-            id: 'dateReleased',
-            header: 'Date Released',
-            filterFn: 'lessThanOrEqualTo',
-            sortingFn: 'datetime',
+        id: 'dateReleased',
+        header: 'Date Released',
+        filterFn: 'lessThanOrEqualTo',
+        sortingFn: 'datetime',
 
-            Cell: ({ cell }) => cell.getValue()?.format('DD/MM/YY'), //render Date as a string
-            Header: ({ column }) => <em>{column.columnDef.header}</em>, //custom header markup
+        Cell: ({ cell }) => cell.getValue()?.format('DD/MM/YY'), //render Date as a string
+        Header: ({ column }) => <em>{column.columnDef.header}</em>, //custom header markup
 
-            //Custom Date Picker Filter from @mui/x-date-pickers
-            Filter: ({ column }) => (
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker
-                  onChange={(newValue) => {
-                    column.setFilterValue(newValue);
-                  }}
+        //Custom Date Picker Filter from @mui/x-date-pickers
+        Filter: ({ column }) => (
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              onChange={(newValue) => {
+                column.setFilterValue(newValue);
+              }}
 
-                  slotProps={{
-                    textField: {
-                      helperText: 'Filter Mode: Less Than',
-                      sx: { minWidth: '120px' },
-                      variant: 'standard',
-                    },
-                  }}
+              slotProps={{
+                textField: {
+                  helperText: 'Filter Mode: Less Than',
+                  sx: { minWidth: '120px' },
+                  variant: 'standard',
+                },
+              }}
 
-                  value={column.getFilterValue()}
-                  format="DD-MM-YYYY"
-                />
-              </LocalizationProvider>
-            )
+              value={column.getFilterValue()}
+              format="DD-MM-YYYY"
+            />
+          </LocalizationProvider>
+        )
       },
       {
         accessorFn: (row) => moment(row.dateAdded, "DD/MM/YYYY"), //convert to Date for sorting and filtering
-            id: 'dateAdded',
-            header: 'Date Added',
-            filterFn: 'lessThanOrEqualTo',
-            sortingFn: 'datetime',
+        id: 'dateAdded',
+        header: 'Date Added',
+        filterFn: 'lessThanOrEqualTo',
+        sortingFn: 'datetime',
 
-            Cell: ({ cell }) => cell.getValue()?.format('DD/MM/YY'), //render Date as a string
-            Header: ({ column }) => <em>{column.columnDef.header}</em>, //custom header markup
+        Cell: ({ cell }) => cell.getValue()?.format('DD/MM/YY'), //render Date as a string
+        Header: ({ column }) => <em>{column.columnDef.header}</em>, //custom header markup
 
-            //Custom Date Picker Filter from @mui/x-date-pickers
-            Filter: ({ column }) => (
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker
-                  onChange={(newValue) => {
-                    column.setFilterValue(newValue);
-                  }}
+        //Custom Date Picker Filter from @mui/x-date-pickers
+        Filter: ({ column }) => (
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              onChange={(newValue) => {
+                column.setFilterValue(newValue);
+              }}
 
-                  slotProps={{
-                    textField: {
-                      helperText: 'Filter Mode: Less Than',
-                      sx: { minWidth: '120px' },
-                      variant: 'standard',
-                    },
-                  }}
+              slotProps={{
+                textField: {
+                  helperText: 'Filter Mode: Less Than',
+                  sx: { minWidth: '120px' },
+                  variant: 'standard',
+                },
+              }}
 
-                  value={column.getFilterValue()}
-                  format="DD-MM-YYYY"
-                />
-              </LocalizationProvider>
-            )
+              value={column.getFilterValue()}
+              format="DD-MM-YYYY"
+            />
+          </LocalizationProvider>
+        )
       },
     ],
     [],
@@ -171,56 +197,70 @@ const CollectionList = () => {
   );
 
   return (
-      <MaterialReactTable
-          title="Collection"
-          columns={columns}
-          data={data}
-          enableRowSelection
-          positionToolbarAlertBanner="bottom" //show selected rows count on bottom toolbar
+    <MaterialReactTable
+      title="Collection"
+      columns={columns}
+      data={data}
+      enableRowSelection
+      positionToolbarAlertBanner="bottom" //show selected rows count on bottom toolbar
 
-          //add custom action buttons to top-left of top toolbar
+      //add custom action buttons to top-left of top toolbar
 
-          renderBottomToolbarCustomActions={({ table }) => {
-            const handleDelete= () => {
-              table.getSelectedRowModel().flatRows.map((row) => {
-                alert('deleting ' + row.getValue('name'));
-              });
+      renderBottomToolbarCustomActions={({ table }) => {
+        const handleDelete = () => {
+          table.getSelectedRowModel().flatRows.map((row) => {
+            const options = {
+              method: 'DELETE',
+              route: "/collection/delete",
+              body: {
+                'id': row.getValue('id'),
+              }
             };
-    
-            return (
-            <Box sx={{ display: 'flex', gap: '1rem', p: '4px' }}>    
-              <Button
-                color="error"
-                // For some reason, button is disabled when all rows selected
-                // TODO: find fix
-                disabled={!table.getIsSomeRowsSelected() && !table.getIsAllRowsSelected()}
-                onClick={() => {
-                  // api call to backend
-                  // setData to new data
-                  handleDelete();
-                }}
-                variant="contained"
-              >
-                Delete selected
-              </Button>
-            </Box>
-            );
-    
-          }}
 
-            //customize built-in buttons in the top-right of top toolbar
+            apiCall(() => {}, options)
+              .then((res) => {
+                if (res) {
+                  // set error msg if api call returns error
 
-            renderToolbarInternalActions={({ table }) => (
+                }
+              });
+          });
+        };
 
-                <Box>
-                    {/* add custom button to print table  */}
-                    <MRT_ToggleDensePaddingButton table={table} />
-                    <MRT_FullScreenToggleButton table={table} />
-                </Box>
-            )}
+        return (
+          <Box sx={{ display: 'flex', gap: '1rem', p: '4px' }}>
+            <Button
+              color="error"
+              // For some reason, button is disabled when all rows selected
+              // TODO: find fix
+              disabled={!table.getIsSomeRowsSelected() && !table.getIsAllRowsSelected()}
+              onClick={() => {
+                // api call to backend
+                // setData to new data
+                handleDelete();
+              }}
+              variant="contained"
+            >
+              Delete selected
+            </Button>
+          </Box>
+        );
 
-        />
-    );
+      }}
+
+      //customize built-in buttons in the top-right of top toolbar
+
+      renderToolbarInternalActions={({ table }) => (
+
+        <Box>
+          {/* add custom button to print table  */}
+          <MRT_ToggleDensePaddingButton table={table} />
+          <MRT_FullScreenToggleButton table={table} />
+        </Box>
+      )}
+
+    />
+  );
 };
 
 
