75cb50eb03d35d5cebd120d4ebb8275a885d1bb2
Uguudei <auudguu@gmail.com>
2023-11-02 15:32:54 +1100

    Added stub page for Manager Feedback

---
+++ frontend/src/pages/ManagerFeedback.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import Typography from '@mui/material/Typography';
+
+
+function ManagerFeedback() {
+  return (
+    <Typography>Manager Feedback Page</Typography>
+  );
+}
+
+
+export default ManagerFeedback;
