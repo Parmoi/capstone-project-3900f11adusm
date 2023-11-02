d4ee30362c4c7b01b6f03193c10d3aee529e3dac
Uguudei <auudguu@gmail.com>
2023-11-02 12:57:55 +1100

    Added a home page unique to managers

---
+++ frontend/src/pages/ManagerHomePage.jsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import { useState, useEffect } from 'react';
+
+import { ProfileBox, ProfileDetailsDisplay, ProfileDetailsEdit, SocialMediaDisplay, SocialMediaEdit } from '../components/ProfileDetails'
+import CollectionCompletion from '../components/CollectionCompletion';
+
+import { apiCall } from '../App';
+
+const theme = createTheme();
+
+function ManagerHomePage() {
+
+  const style = {
+    alignItems: 'center', 
+    marginTop: "20px", 
+    backgroundColor: "White",
+    borderRadius: '4px'
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" sx={{ py: 6, }} maxWidth="lg">
+      <CssBaseline/>
+        <Grid container spacing={2}>
+          <Grid item xs={4}>
+            
+          </Grid>
+
+          <Grid item xs={4}>
+
+          </Grid>
+
+          <Grid item xs={4}>
+
+          </Grid>
+
+        </Grid>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+
+export default ManagerHomePage;
