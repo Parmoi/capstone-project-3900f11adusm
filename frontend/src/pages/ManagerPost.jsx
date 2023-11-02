94fd1ca4c493604927f616c6a926d7fa7cfa2a91
Uguudei <auudguu@gmail.com>
2023-11-02 15:33:34 +1100

    Added stub page for Manager Post

---
+++ frontend/src/pages/ManagerPost.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import Typography from '@mui/material/Typography';
+
+
+function ManagerPost() {
+  return (
+    <Typography>Manager Post Page</Typography>
+  );
+}
+
+
+export default ManagerPost;
