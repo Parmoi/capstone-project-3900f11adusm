f115ba15f0944c75b5a75af85ad0ecad8ce558c6
Uguudei <auudguu@gmail.com>
2023-11-02 15:32:34 +1100

    Added stub page for Manager Analytics

---
+++ frontend/src/pages/ManagerAnalytics.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import Typography from '@mui/material/Typography';
+
+
+function ManagerAnalytics() {
+  return (
+    <Typography>Manager Analytics</Typography>
+  );
+}
+
+
+export default ManagerAnalytics;
