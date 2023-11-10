9aa58afc113e0be67bc25a9c39b4c6cfb32a7ae2
Uguudei <auudguu@gmail.com>
2023-11-10 16:43:31 +1100

    Added Admin Pages

---
+++ frontend/src/pages/Admin/AdminManageCollectors.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+import Typography from '@mui/material/Typography';
+
+
+
+function AdminManageCollectors() {
+  return (
+    <Typography>Manage Collectors</Typography>
+  );
+}
+
+
+export default AdminManageCollectors;
---
+++ frontend/src/pages/Admin/AdminManageManagers.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+import Typography from '@mui/material/Typography';
+
+
+
+function AdminManageManagers() {
+  return (
+    <Typography>Manage Managers</Typography>
+  );
+}
+
+
+export default AdminManageManagers;
